7dc170565081b30dedbbd1842acbb1814b38e1a2
Aydon Smith <aydonjoe.smith@gmail.com>
2025-11-11 21:22:04 +0000

    Add validators to CashflowRecord model to handle input data issues

--- src/fund_analysis/models.py
+++ src/fund_analysis/models.py
@@ -1,8 +1,12 @@
 import datetime
+from typing import Any
 
-from pydantic import BaseModel, Field
+from dateutil import parser
+from pydantic import BaseModel, Field, field_validator
 
 from fund_analysis.enums import CashflowType, Currency
+
+CURRENCY_TYPO_MAP = {"GPB": "GBP"}
 
 
 class CashflowRecord(BaseModel):
@@ -18,6 +22,19 @@ class CashflowRecord(BaseModel):
     cashflow_amount_local: float = Field(alias="Cashflow Amount Local")
     cashflow_amount_base: float = Field(alias="Cashflow Amount Base")
     base_currency: Currency = Field(alias="Base Currency")
+
+    @field_validator("local_currency", "base_currency", mode="before")
+    def correct_currency_typos(cls, v: Any) -> Any:
+        if v in CURRENCY_TYPO_MAP:
+            return CURRENCY_TYPO_MAP[v]
+        return v
+
+    @field_validator("date", mode="before")
+    def clean_dates(cls, v: Any) -> Any:
+        if isinstance(v, str):
+            clean = v.strip().replace("`", "")
+            return parser.parse(clean, dayfirst=True)
+        return v
 
     class Config:
         populate_by_name = True
